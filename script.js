6b9eb25f90ceaab79d7d904f1dd07d94caa343b0
mahir-b0 <120801710+mahir-b0@users.noreply.github.com>
2024-10-31 12:51:53 +1100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function validateFlag() {
     const correctFlag = "t3n_gr4nd_r4nd0mh4sh"; // go ahead! you won't last a second past this point.
   
     if (userInput === correctFlag) {
-      window.location.href = "https://example.com";
+      window.location.href = "https://pinocchio-gl9j.onrender.com/";
     } else {
       alert("Incorrect flag. Please try again.");
     }
